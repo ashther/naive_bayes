9cc39ae240e8208516f587c89c1005a5f33b3a65
ashther <ashther@qq.com>
2015-09-17 09:18:43 +0800

    统一了离散和连续变量的算法

--- naive_bayes.py
+++ naive_bayes.py
@@ -9,58 +9,77 @@ import random
 from sklearn.naive_bayes import GaussianNB
 from dataSource import *
 
-def trainCreate(data, res):
-    num = random.randint(0, len(data)-1)
-    ind = np.hstack((np.arange(num), np.arange(num+1, len(data))))
-    train = np.column_stack((data, res))[ind]
-    test = data[num]
-    result = res[num]
-    return train, test, result
+#def trainCreate(data, res):
+#    num = random.randint(0, len(data)-1)
+#    ind = np.hstack((np.arange(num), np.arange(num+1, len(data))))
+#    train = np.column_stack((data, res))[ind]
+#    test = data[num]
+#    result = res[num]
+#    return train, test, result
     
 def densityProbCaculate(x, miu, sigma):
     return 1 / (np.sqrt(2*np.pi) * sigma * np.exp((x-miu)**2/(2*sigma**2)))
 
-def continuousBayes(train, test):
+#def continuousBayes(train, test):
+#    category = set(train[:, -1])
+#    max_prob = 0
+#    for ctg in category:
+#        data_ctg = train[train[:, -1] == ctg, :-1].astype(float)
+#        prob_ctg = float(len(data_ctg)) / float(len(train))
+#        prob_feature = reduce(lambda x,y:x*y, 
+#                              map(densityProbCaculate, test, 
+#                                  data_ctg.mean(axis=0), data_ctg.std(axis=0, ddof=1)))
+#        prob = prob_ctg * prob_feature
+#        if prob > max_prob:
+#            max_prob, result = prob, ctg
+#    return result
+   
+def probCaculate(s, items):
+    return ((items == s).sum() + 1.) / len(items)
+
+#def discreteBayes(train, test):
+#    category = set(train[:, -1])
+#    max_prob = 0
+#    for ctg in category:
+#        data_ctg = train[train[:, -1] == ctg, :-1]
+#        prob_ctg = float(len(data_ctg)) / float(len(train))
+#        prob_feature = reduce(lambda x,y:x*y, 
+#                              map(probCaculate, test, data_ctg.transpose()))
+#        prob = prob_ctg * prob_feature
+#        print ctg, prob_ctg, prob_feature, prob
+#        if prob > max_prob:
+#            max_prob, result = prob, ctg
+#    print result
+#    return result
+    
+def naiveBayes(train, test, method):
     category = set(train[:, -1])
     max_prob = 0
     for ctg in category:
-        data_ctg = train[train[:, -1] == ctg, :-1].astype(float)
+        data_ctg = train[train[:, -1] == ctg, :-1]
         prob_ctg = float(len(data_ctg)) / float(len(train))
-        prob_feature = reduce(lambda x,y:x*y, 
+        if method == 'dis':
+            prob_feature = reduce(lambda x,y:x*y, 
+                                  map(probCaculate, test, data_ctg.transpose()))
+        else:
+            data_ctg = data_ctg.astype(float)
+            prob_feature = reduce(lambda x,y:x*y, 
                               map(densityProbCaculate, test, 
                                   data_ctg.mean(axis=0), data_ctg.std(axis=0, ddof=1)))
         prob = prob_ctg * prob_feature
         if prob > max_prob:
             max_prob, result = prob, ctg
     return result
-   
-def probCaculate(s, items):
-    return ((items == s).sum() + 1.) / len(items)
 
-def discreteBayes(train, test):
-    category = set(train[:, -1])
-    max_prob = 0
-    for ctg in category:
-        data_ctg = train[train[:, -1] == ctg, :-1]
-        prob_ctg = float(len(data_ctg)) / float(len(train))
-        prob_feature = reduce(lambda x,y:x*y, 
-                              map(probCaculate, test, data_ctg.transpose()))
-        prob = prob_ctg * prob_feature
-        print ctg, prob_ctg, prob_feature, prob
-        if prob > max_prob:
-            max_prob, result = prob, ctg
-    print result
-    return result
-
-def crossValidation(data, res, k, method):
-    precision = np.zeros(k)
-    for i in range(k):
-        train, test, result = trainCreate(data, res)
-        predict = discreteBayes(train, test) if method == 'dis' else continuousBayes(train, test)
-        if predict == result:
-            precision[i] = 1
-    return precision.mean()
-    
+#def crossValidation(data, res, k, method):
+#    precision = np.zeros(k)
+#    for i in range(k):
+#        train, test, result = trainCreate(data, res)
+#        predict = discreteBayes(train, test) if method == 'dis' else continuousBayes(train, test)
+#        if predict == result:
+#            precision[i] = 1
+#    return precision.mean()
+  
 def eachTest(data, res, method):
     correct = 0
     for i, da in enumerate(data):
@@ -68,7 +87,7 @@ def eachTest(data, res, method):
         train = np.column_stack((data, res))[ind]
         test = da
         result = res[i]
-        predict = discreteBayes(train, test) if method == 'dis' else continuousBayes(train, test)       
+        predict = naiveBayes(train, test, method)       
         if predict == result:
             correct += 1
     return float(correct) / len(data)
@@ -83,7 +102,7 @@ def skTest(data, res):
         result = res[i]
         clf = GaussianNB()     
         clf.fit(train, train_result)
-        predict = clf.predict(da)[0]       
+        predict = clf.predict(test)[0]       
         if predict == result:
             correct += 1
     return float(correct) / len(data)
